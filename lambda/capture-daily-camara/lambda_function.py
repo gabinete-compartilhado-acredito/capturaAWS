828081389a9bd1f9f3e30d9c6e06460a08ab7c4e
Henrique S. Xavier <hsxavier@gmail.com>
2021-08-30 12:16:14 -0300

    Refatorada capture-daily para facilitar inclusão de novos triggers de capturas, e inclusão da captura de tweeets na lista.

--- lambda/capture-daily-camara/lambda_function.py
+++ lambda/capture-daily-camara/lambda_function.py
@@ -1,50 +1,30 @@
 import boto3
 import json
 
-def lambda_handler(event, context):
-    
+def invoke_lambda(dynamo_table, capture_type, begins_with):
+
     client = boto3.client('lambda')
     
-    # Start capture of camara data:
-    dynamo_table = 'capture_urls'
-    capture_type = 'daily'
-    begins_with  = 'camara'
+    # Call lambda function:
     response = client.invoke(
-            FunctionName='call-step-functions',
-            InvocationType='Event',
-            Payload= json.dumps({'dynamo_table':dynamo_table, 'capture_type': capture_type,'begins_with': begins_with}))
-    print(dynamo_table + ': Triggered '+begins_with+' with response:')
-    print(response)
-        
-    # Start capture of senado data:  
-    dynamo_table = 'capture_urls'
-    capture_type = 'daily'
-    begins_with  = 'senado'
-    response = client.invoke(
-            FunctionName='call-step-functions',
-            InvocationType='Event',
-            Payload= json.dumps({'dynamo_table':dynamo_table, 'capture_type': capture_type, 'begins_with': begins_with}))
+        FunctionName='call-step-functions',
+        InvocationType='Event',
+        Payload= json.dumps({'dynamo_table':dynamo_table, 'capture_type': capture_type, 'begins_with': begins_with}))
+    
+    # Log:
     print(dynamo_table + ': Triggered '+begins_with+' with response:')
     print(response)
 
-    # Start capture of executivo data:
-    dynamo_table = 'capture_urls'
-    capture_type = 'daily'
-    begins_with  = 'executivo'
-    response = client.invoke(
-            FunctionName='call-step-functions',
-            InvocationType='Event',
-            Payload= json.dumps({'dynamo_table':dynamo_table, 'capture_type': capture_type, 'begins_with': begins_with}))
-    print(dynamo_table + ': Triggered '+begins_with+' with response:')
-    print(response)
 
-    # Start processing data with python:
-    dynamo_table = 'python_process'
-    capture_type = 'daily'
-    begins_with  = ''
-    response = client.invoke(
-            FunctionName='call-step-functions',
-            InvocationType='Event',
-            Payload= json.dumps({'dynamo_table':dynamo_table, 'capture_type': capture_type, 'begins_with': begins_with}))
-    print(dynamo_table + ': Triggered '+begins_with+' with response:')
-    print(response)
+def lambda_handler(event, context):
+    
+    # Seleção de serviços de captura (configurados no DynamoDB) que serão rodados diariamente:
+    capture_services = [{'dynamo_table': 'capture_urls', 'capture_type':'daily', 'begins_with':'camara'},
+                        {'dynamo_table': 'capture_urls', 'capture_type':'daily', 'begins_with':'senado'},
+                        {'dynamo_table': 'capture_urls', 'capture_type':'daily', 'begins_with':'executivo'},
+                        {'dynamo_table': 'python_process', 'capture_type':'daily', 'begins_with':''},
+                        {'dynamo_table': 'capture_urls', 'capture_type':'daily', 'begins_with':'twitter'}]
+    
+    # Loop de ativação das capturas listadas acima:
+    for service in capture_services:
+        invoke_lambda(service['dynamo_table'], service['capture_type'], service['begins_with'])
